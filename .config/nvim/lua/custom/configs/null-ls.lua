7af9891d71742a4eac8ca2cd78b0c92c6e294095
r.fabre <r.fabre@criteo.com>
2023-11-25 13:34:23 +0000

    Update dotfiles

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -4,7 +4,9 @@ local opts = {
   sources = {
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.pylint,
-    null_ls.builtins.formatting.black.with({extra_args = {"--line-length 120"}}),
+    null_ls.builtins.formatting.black.with({
+          extra_args = {"--line-length=120", "--skip-string-normalization"},
+        }),
     null_ls.builtins.formatting.isort,
   },
   on_attach = function(client, bufnr)
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -46,12 +46,67 @@ local plugins = {
     end,
   },
   {
+    'mfussenegger/nvim-dap'
+  },
+  {
     "scalameta/nvim-metals",
-    name = "metals",
+    dependencies = { "nvim-lua/plenary.nvim" },
     ft = { "scala", "sbt", "java" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+    -- stylua: ignore
+    keys = {
+      {'<leader>me', function() require "telescope".extensions.metals.commands() end, desc="Metals commands"},
+      {'<leader>mc', function() require "metals".compile_cascade() end, desc="Metals compile cascade"},
+      {'<leader>mw', function() vim.lsp.buf.add_workspace_folder() end, desc="Metals add workspace"},
+      {'<leader>gd', function() vim.lsp.buf.definition() end, desc="Metals goto definition"}
     },
-  }
+    config = function()
+      local metals = require("metals")
+      local config = metals.bare_config()
+
+      config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+        serverProperties = { "-Xmx2g" },
+        serverVersion = "latest.snapshot",
+        enableSemanticHighlighting = false,
+      }
+
+      config.init_options.statusBarProvider = "on"
+
+      -- cmp integration
+      config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      local dap = require("dap")
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+          },
+        },
+      }
+      dap.listeners.after["event_terminated"]["nvim-metals"] = function()
+        -- vim.notify("Tests have finished!")
+        dap.repl.open()
+      end
+
+      config.on_attach = function(client, bufnr)
+        metals.setup_dap()
+        require("lsp-format").on_attach(client, bufnr)
+      end
+
+      -- Autocmd that will actually be in charge of starting the whole thing
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+          metals.initialize_or_attach(config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
 }
 return plugins
